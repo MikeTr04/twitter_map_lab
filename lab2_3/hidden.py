5a1d262f98dafbb71f24f63aa995fe78e249007b
MikeTr04 <regularinternetuser@gmail.com>
2022-02-18 23:54:24 +0200

    Final commit

--- lab2_3/hidden.py
+++ lab2_3/hidden.py
@@ -13,7 +13,3 @@ def oauth():
     {'bearer_token': 'AAAAAAAAAAAAAAAAAAAAAAsyZQEAAAAAqvzT%2FpQVsm0d484gvxOmWU8dAbw%3DnXwRun3mdC1KgOVtPNVdJuTRuomk4n7yk4Z1jQYKypYC6XfEc3'}
     """
     return {"bearer_token": "AAAAAAAAAAAAAAAAAAAAAAsyZQEAAAAAqvzT%2FpQVsm0d484gvxOmWU8dAbw%3DnXwRun3mdC1KgOVtPNVdJuTRuomk4n7yk4Z1jQYKypYC6XfEc3"} # pylint: disable=C0116
-
-
-if __name__ == "__main__":
-    doctest.testmod()
